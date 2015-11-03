932ec8f43e5d2a7e40417c8a28f1015105e09ef2
george420 <george.github@gmail.com>
2015-11-03 11:20:20 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,9 @@
 subject_train <- read.table("./train/subject_train.txt")
 x_train <- read.table("./train/x_train.txt")
 y_train <- read.table("./train/y_train.txt")
-subject_test <- read.table("./test/subject_train.txt")
-x_test <- read.table("./test/x_train.txt")
-y_test <- read.table("./test/y_train.txt")
+subject_test <- read.table("./test/subject_test.txt")
+x_test <- read.table("./test/x_test.txt")
+y_test <- read.table("./test/y_test.txt")
 
 subject <- rbind(subject_train, subject_test)
 x <- rbind(x_train, x_test)
